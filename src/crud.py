5002b97b040eb933c4092e80a85ca53fface4157
Sosuke Hosokawa <so298go@gmail.com>
2022-10-22 05:19:18 +0900

    not run async

--- src/crud.py
+++ src/crud.py
@@ -1,27 +1,33 @@
 from datetime import datetime, timedelta
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
-from . import models, schemas
+from . import schemas
+from .models import User, TimeData
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+async def get_user(db: AsyncSession, user_id: int):
+    ret = await db.execute(select(User).filter(User.id == user_id))
+    return ret.first()
 
 
-def get_user_by_shown_id(db: Session, shown_id: str):
-    return db.query(models.User).filter(models.User.shown_id == shown_id).first()
+async def get_user_by_shown_id(db: AsyncSession, shown_id: str):
+    ret = await db.execute(select(User).filter(User.shown_id == shown_id))
+    return ret.first()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+async def get_users(db: AsyncSession, skip: int = 0, limit: int = 100):
+    ret = await db.execute(select(User).offset(skip).limit(limit))
+    return ret.all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(shown_id=user.shown_id)
+async def create_user(db: AsyncSession, user: schemas.UserCreate):
+    db_user = User(shown_id=user.shown_id)
     db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    await db.commit()
+    await db.refresh(db_user)
     return db_user
 
 # def get_sensors(db: Session, skip: int = 0, limit: int = 100):
@@ -38,26 +44,27 @@ def create_user(db: Session, user: schemas.UserCreate):
 #     return db_sensor
 
 
-def get_timedata(db: Session, sensor_id: int, skip: int = 0, limit: int = 100):
-    return db.query(models.TimeData).filter(models.TimeData.id == sensor_id).offset(skip).limit(limit).all()
+async def get_timedata(db: AsyncSession, sensor_id: int, skip: int = 0, limit: int = 100):
+    ret = await db.execute(select(TimeData).filter(TimeData.id == sensor_id).offset(skip).limit(limit))
+    return ret.all()
 
-def get_timedata_mean(db: Session, sensor_id: int, days: int, timestep: int, offset_day: int = 0):
+async def get_timedata_mean(db: AsyncSession, sensor_id: int, days: int, timestep: int, offset_day: int = 0):
     now = datetime.now() - timedelta(offset_day)
 
     ret = []
     for i in range(timestep, 0, -1):
         time = now - timedelta((1 - ((i - 1) / timestep)) * days)
         time_next = now - timedelta((1 - (i / timestep)) * days)
-        datas = db.query(models.TimeData).filter(models.TimeData.id == sensor_id).filter(models.TimeData.timestamp > time).filter(models.TimeData.timestamp < time_next).all()
+        datas = await db.execute(select(TimeData).filter(TimeData.id == sensor_id).filter(TimeData.timestamp > time).filter(TimeData.timestamp < time_next))
         mean: float = 0
-        for data in datas:
+        for data in datas.all():
             mean += data.value
         if len(datas) > 0:
             mean /= len(datas)
         ret.append(schemas.TimeData(id=sensor_id, timestamp=time, value=mean))
     return ret
 
-def get_sitting_time(db: Session, sensor_id: int, days: int, timestep: int, offset_day: int = 0, limit: float = 10):
+async def get_sitting_time(db: AsyncSession, sensor_id: int, days: int, timestep: int, offset_day: int = 0, limit: float = 10) -> List[schemas.SittingData]:
     now = datetime.now() - timedelta(offset_day)
     time_per_step = days / timestep * 24
 
@@ -65,19 +72,22 @@ def get_sitting_time(db: Session, sensor_id: int, days: int, timestep: int, offs
     for i in range(timestep, 0, -1):
         start = now - timedelta((1 - ((i - 1) / timestep)) * days)
         end = now - timedelta((1 - (i / timestep)) * days)
-        query = db.query(models.TimeData).filter(models.TimeData.id == sensor_id).filter(models.TimeData.timestamp > start).filter(models.TimeData.timestamp < end)
-        datas_all = query.all()
-        datas_sitting = query.filter(models.TimeData.value > limit).all()
+        query_all = select(TimeData).filter(TimeData.id == sensor_id).filter(TimeData.timestamp > start).filter(TimeData.timestamp < end)
+        query_sitting = query_all.filter(TimeData.value > limit)
+        datas_all_promise = db.execute(query_all)
+        datas_sitting_promise = db.execute(query_sitting)
+        datas_all = (await datas_all_promise).all()
+        datas_sitting = (await datas_sitting_promise).all()
         hours: float = 0
         if len(datas_all) > 0:
             hours = time_per_step * (len(datas_sitting) / len(datas_all))
         ret.append(schemas.SittingData(start=start, end=end, hours=hours, id=sensor_id))
     return ret
 
-def create_timedata(db: Session, timedata: schemas.TimeDataCreate) -> List[schemas.SittingData]:
+async def create_timedata(db: AsyncSession, timedata: schemas.TimeDataCreate):
     now = datetime.now()
-    db_timedata = models.TimeData(**timedata.dict(), timestamp=now)
+    db_timedata = TimeData(**timedata.dict(), timestamp=now)
     db.add(db_timedata)
-    db.commit()
-    db.refresh(db_timedata)
+    await db.commit()
+    await db.refresh(db_timedata)
     return db_timedata
--- src/database.py
+++ src/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .env_vars import DATABASE_URI
 
-engine = create_engine(
+engine = create_async_engine(
     DATABASE_URI, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, _class=AsyncSession)
 
 Base = declarative_base()
